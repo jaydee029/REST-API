1d2299877c82dc213884f3ef3b523b5e5be8376e
DEEJAY29 <dhruvj797@gmail.com>
2023-08-04 19:59:09 +0530

    REST-API

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express=require("express")
+const path=require("path")
+const recipesrouter=require("./router/recipes.js")
+const usersrouter=require("./router/users.js")
+const app=express()
+const cors= require("cors")
+
+app.use(cors())
+
+app.use((req,res,next)=>{
+    const {method,path}=req;
+    console.log(`New Request to ${method}${path} at ${new Date().toISOString()}`)
+    next()
+})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+    res.redirect("/api/src/recipes");
+});
+
+app.use('/api/src/recipes',recipesrouter)
+app.use('/api/src/users',usersrouter)
+app.use(auth.initialize())
+
+const port =process.env.PORT ||8080
+
+app.listen(port,()=>{
+    console.log(`The server is up at port ${port}.`)
+})
